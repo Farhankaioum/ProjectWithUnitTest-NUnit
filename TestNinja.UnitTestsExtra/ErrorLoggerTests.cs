d8a7eb862c91134cec158b89adcecf36d4655e45
FarhanKaioum <km1472233@gmail.com>
2020-07-23 12:28:16 +0600

    ErrorLogger Class tested

---
+++ TestNinja.UnitTestsExtra/ErrorLoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTestsExtra
+{
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            // Arrange
+            var logger = new ErrorLogger();
+            var errorMessage = "a";
+
+            // Act
+            logger.Log(errorMessage);
+
+            // Assert
+            Assert.That(logger.LastError, Is.EqualTo(errorMessage));
+        }
+    }
+}
