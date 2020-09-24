2588bc5a58994acc44ec4ec09a4d2c314e8bf045
Farhan Kaioum <km1472233@gmail.com>
2020-09-23 18:26:15 -0700

    EmployeeController test

---
+++ TestNinja.UnitTestsExtra/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTestsExtra.Mocking
+{
+    public class EmployeeControllerTests
+    {
+        private Mock<IEmployeeStorage> _employeeStroge;
+        private EmployeeController _employeeController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _employeeStroge = new Mock<IEmployeeStorage>();
+            _employeeController = new EmployeeController(_employeeStroge.Object);
+        }
+
+        [Test]
+        public void DeleteEmployee_InvalidId_ReturnEmpty()
+        {
+            // arrange
+
+            // act
+            _employeeController.DeleteEmployee(1);
+
+            // assert
+            _employeeStroge.Verify(s => s.DeleteEmployee(1));
+        }
+    }
+}
