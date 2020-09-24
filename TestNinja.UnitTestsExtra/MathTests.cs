302353b34228f283fdbb7c0a32c6be881457ee2f
Farhan Kaioum <km1472233@gmail.com>
2020-09-24 00:50:46 -0700

    update math test

--- TestNinja.UnitTestsExtra/MathTests.cs
+++ TestNinja.UnitTestsExtra/MathTests.cs
@@ -1,8 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Math = TestNinja.Fundamentals.Math;
 
 namespace TestNinja.UnitTestsExtra
